26c299c4b41c84fd6fa239b070df3f7482533873
tanakit-saen <64766374+tanakit-saen@users.noreply.github.com>
2020-05-04 12:04:55 +0700

    Add files via upload

---
+++ oop/oop/src/oop/Oop.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop;
+
+/**
+ *
+ * @author jay
+ */
+public class Oop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
